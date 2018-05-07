3ea45cfc8b8f0d512bc7a0054a2f012daa692c1a
sreekar1 <sreekarvemula@gmail.com>
2018-05-07 10:04:01 +0530

    Add files via upload

---
+++ Winforms SQLite/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Winforms_SQLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSelectTable = new System.Windows.Forms.Button();
+            this.CmbTables = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnSelectTable
+            // 
+            this.BtnSelectTable.Location = new System.Drawing.Point(205, 12);
+            this.BtnSelectTable.Name = "BtnSelectTable";
+            this.BtnSelectTable.Size = new System.Drawing.Size(75, 23);
+            this.BtnSelectTable.TabIndex = 0;
+            this.BtnSelectTable.Text = "Ok";
+            this.BtnSelectTable.UseVisualStyleBackColor = true;
+            this.BtnSelectTable.Click += new System.EventHandler(this.BtnSelectTable_Click);
+            // 
+            // CmbTables
+            // 
+            this.CmbTables.FormattingEnabled = true;
+            this.CmbTables.Location = new System.Drawing.Point(93, 14);
+            this.CmbTables.Name = "CmbTables";
+            this.CmbTables.Size = new System.Drawing.Size(106, 21);
+            this.CmbTables.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a table:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(554, 237);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 289);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmbTables);
+            this.Controls.Add(this.BtnSelectTable);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSelectTable;
+        private System.Windows.Forms.ComboBox CmbTables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Winforms SQLite/Form1.cs
@@ -0,0 +1,129 @@
+﻿// (c) Rou1997
+// E-mail: rou1997<<bow-bow!>>bk<<point>>ru
+// License: Public Domain | MIT
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+// ------------------------------
+using System.Data.SQLite;
+
+namespace Winforms_SQLite
+{
+    public partial class Form1 : Form
+    {
+        string mDbPath = Application.StartupPath + "/getstarted.db";
+
+        SQLiteConnection mConn;
+        SQLiteDataAdapter mAdapter;
+        DataTable mTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // -------------------- Connecting To DB --------------------
+            // ----------------------------------------------------------
+            // If DB Not Exists, it will be created.
+            mConn = new SQLiteConnection("Data Source=" + mDbPath);
+
+            // ----------------- Opening The Connection -----------------
+            // ----------------------------------------------------------
+            // I.e. Opening DB's File for Reading And Writing.
+            // SQLiteDataAdapter cans do it automatically.
+            // But, if you would also use SQLiteCommand, or GetSchema(),
+            // you should Open DB Manually.
+            mConn.Open();
+
+            // ---------- Creating A Test Table, If Not Exists ----------
+            // ----------------------------------------------------------
+            // id        - Unique Counter - Key Field (Required in any table)
+            // FirstName - Text
+            // Age       - Integer
+            using (SQLiteCommand mCmd = new SQLiteCommand("CREATE TABLE IF NOT EXISTS [Test Table] (id INTEGER PRIMARY KEY AUTOINCREMENT, 'FirstName' TEXT, 'Age' INTEGER);", mConn))
+            {
+                mCmd.ExecuteNonQuery();
+            }
+
+            // ---------- Get All Tables From DB to ComboBox -----------
+            // ---------------------------------------------------------
+            // There "Tables" is a system table which contains info
+            // about tables in DB.
+            // "TABLE_NAME" field in "Tables" contains table names.
+            using (DataTable mTables = mConn.GetSchema("Tables"))
+            {
+                for (int i = 0; i < mTables.Rows.Count; i++)
+                {
+                    CmbTables.Items.Add(mTables.Rows[i].ItemArray[mTables.Columns.IndexOf("TABLE_NAME")].ToString());
+                }
+
+                if (CmbTables.Items.Count > 0)
+                {
+                    CmbTables.SelectedIndex = 0; // Default selected index.
+                }
+            }
+        }
+
+        private void BtnSelectTable_Click(object sender, EventArgs e)
+        {
+            // --- Putting All Data From Selected Table To DataTable ---
+            // ---------------------------------------------------------
+            // In simply put, DataTable is just matrix (2-dimensional array)
+            // which stores data of the table.
+            mAdapter = new SQLiteDataAdapter("SELECT * FROM [" + CmbTables.Text + "]", mConn);
+            mTable = new DataTable(); // Don't forget initialize!
+            mAdapter.Fill(mTable);
+
+            // ---------- Disabling Counter Field For Edition ----------
+            // ---------------------------------------------------------
+            // Because it can throw exception.
+            if (mTable.Columns.Contains("id"))
+            {
+                mTable.Columns["id"].ReadOnly = true;
+            }
+
+            // ------------ Making DataBase Saving Changes -------------
+            // ---------------------------------------------------------
+            // SQLiteCommandBuilder authomatically generates
+            // neccessary INSERT, UPDATE, DELETE SQL queries.
+            // Next we just have to run the
+            // mAdapter.Update(mTable);
+            // and all changes in the table will be saved to DataBase.
+            new SQLiteCommandBuilder(mAdapter);
+
+            // ----------- Binding DataTable To DataGridView -----------
+            // ---------------------------------------------------------
+            // DataGridView visualizes DataTable's data in the window.
+            dataGridView1.DataSource = mTable;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // -------- Saving Modified Data To Selected Table ---------
+            // -------------------- On Form Closed ---------------------
+            // ---------------------------------------------------------
+
+            if (mAdapter == null) // If No Table Selected.
+                return;
+
+            mAdapter.Update(mTable);
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            // - Preventing PEEESKY DataGridView Default Error Dialog --
+            // ---- When we trying set a non-int value to int field ----
+            // ---------------------------------------------------------
+            // Just handle DataError event. It's enough.
+            // But if you want, you can to show your own error message.
+            // Not so peeeeesky, please!!! :)
+        }
+    }
+}
